56a3b8abc4ad3f39735cf06908a45c22e32f74ed
Grigorii Horos <horosgrisa@gmail.com>
2020-03-15 01:13:23 +0200

    Fix fns

--- mysql-colorize.plugin.zsh
+++ mysql-colorize.plugin.zsh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
+# Standarized $0 handling, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
-alias mysql="$(whence mysql) --pager=${0:a:h}/mycat"
+alias mysql="command mysql --pager=${0:h}/mycat"
